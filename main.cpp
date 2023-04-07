d67f4c49148b8bfa4b3479d9908f2dd9492c58cc
Joe Coon <jwbcoon@gmail.com>
2023-04-07 00:13:08 -0700

    Moved some stuff around and renamed it while attempting to resolve last errors in code, but failed. Committing in case this presentation of the code is desired.

--- main.cpp
+++ main.cpp
@@ -17,55 +17,64 @@ using namespace std;
 // Function Prototypes
 void initFiles(ifstream& infile, ofstream& outfile, int argc, char* argv[]);
 vector<int> lineReaderInt(int numInputs, ifstream& in);
-bool checkForIntersect(int r, int centerX, int centerY, int x1, int y1, int x2, int y2);
-bool checkForLineIntersect(int firstX1, int firstY1, int firstX2, int firstY2, // First line
-                           int secondX1, int secondY1, int secondX2, int secondY2, int rad, int cx, int cy); // Second line
+bool verifyCircleIntersect(int r, int centerX, int centerY, int x1, int y1, int x2, int y2);
+bool verifyLineIntersect(int firstX1, int firstY1, int firstX2, int firstY2, // First line
+                           int secondX1, int secondY1, int secondX2, int secondY2); // Second line
+pair<double, double> calcIntersectionPoint(int firstX1, int firstY1, int firstX2, int firstY2,
+                                           int secondX1, int secondY1, int secondX2, int secondY2);
+double getDist(double x1, double y1, double x2, double y2);
 
 // Main
 int main(int argc, char* argv[]) {
     // Variable defs
     ifstream infile; ofstream outfile;
     string line;
-    int numIntersects = 1, numIntersectsTmp = 0;
-    vector<int> CI, LI; // case inputs; line inputs
+    int numAreas = 1, numAreasTmp = 0;
+    vector<int> caseInputs, lineInputs; // case inputs; line inputs
     vector<vector<int>> lineStorage;
     // Open files
     initFiles(infile, outfile, argc, argv);
     // File read
     while (!infile.eof()){
         // Get each line of the initial input
-        CI = lineReaderInt(4, infile); // Input order: rad, center x, center y, number of lines
-        for (int i : CI)
+        caseInputs = lineReaderInt(4, infile); // Input order: rad, center x, center y, number of lines
+        int rad = caseInputs[0], cx = caseInputs[1], cy = caseInputs[2], numCases = caseInputs.at(3);
+        for (int i : caseInputs)
             cout << i << " ";
         cout << "\n";
 
         // Loop through each line to check against the given circle
-        for (int i = 0; i < CI.at(3); i++) {
+        for (int i = 0; i < numCases; i++) {
             // Get the line coordinates
-            LI = lineReaderInt(4, infile); // Input order: x1, y1, x2, y2
-            for (int j : LI)
+            lineInputs = lineReaderInt(4, infile); // Input order: x1, y1, x2, y2
+            int x1 = lineInputs[0], y1 = lineInputs[1], x2 = lineInputs[2], y2 = lineInputs[3];
+            for (int j : lineInputs)
                 cout << j << " ";
             cout << "\n";
             // Check for intersection (w/ circle)
-            if (checkForIntersect(CI[0], CI[1], CI[2], LI[0], LI[1], LI[2], LI[3])){
-                lineStorage.push_back(LI); // Store line coordinates for intersection checking
-                numIntersectsTmp++;
+            if (verifyCircleIntersect(rad, cx, cy, x1, y1, x2, y2)) {
+                lineStorage.push_back(lineInputs); // Store line coordinates for intersection checking
+                numAreasTmp++;
             }
+
             // Check for intersection (w/ other lines)
             for (auto & j : lineStorage){
-                if (checkForLineIntersect(LI[0], LI[1], LI[2], LI[3], j[0], j[1], j[2], j[3], CI[0], CI[1], CI[2])){
-                    numIntersectsTmp *= 2;
+                pair<double, double> tempPoint = calcIntersectionPoint(
+                        x1, y1, x2, y2, j[0], j[1], j[2], j[3]);
+                if (verifyLineIntersect(x1, y1, x2, y2, j[0], j[1], j[2], j[3])
+                    && getDist(tempPoint.first, tempPoint.second, cx, cy) <= rad) {
+                    numAreasTmp++;
                 }
             }
-            numIntersects += numIntersectsTmp;
-            numIntersectsTmp = 0;
-            LI.clear(); // Just in case
+            numAreas += numAreasTmp;
+            numAreasTmp = 0;
+            lineInputs.clear(); // Just in case
         }
-        if (!(CI[0] == 0 && CI[1] == 0 && CI[2] == 0 && CI[3] == 0)){
-            outfile << numIntersects << "\n";
+        if (!(rad == 0 && cx == 0 && cy == 0 && numCases == 0)) {
+            outfile << numAreas << "\n";
         }
-        CI.clear(); // Just in case
-        numIntersects = 1;
+        caseInputs.clear(); // Just in case
+        numAreas = 1;
         lineStorage.clear();
     }
     // Cleanup before program end
@@ -74,40 +83,48 @@ int main(int argc, char* argv[]) {
 }
 
 // Check if lines intersect with circle
-bool checkForIntersect(int r, int centerX, int centerY, int x1, int y1, int x2, int y2){
-    float area2 = abs((x2 - x1) * (centerY - y1) - (centerX - x1) * (y2 - y1)); // Triangle area * 2
-    float dist = area2 / sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2)); // Distance as a function of area * 2 / segment length
+bool verifyCircleIntersect(int r, int centerX, int centerY, int x1, int y1, int x2, int y2) {
+    double area2 = abs((x2 - x1) * (centerY - y1) - (centerX - x1) * (y2 - y1)); // Triangle area * 2
+    double dist = area2 / sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2)); // Distance as a function of area * 2 / segment length
     if (dist <= r) // Check if distance is smaller than radius
         return true;
     return false;
 }
 
 // Calculate the distance between two points
-float getDist(int x1, int y1, int x2, int y2){
+double getDist(double x1, double y1, double x2, double y2) {
     return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 }
 
 // Check if lines intersect with each other
-bool checkForLineIntersect(int firstX1, int firstY1, int firstX2, int firstY2, int secondX1, int secondY1, int secondX2, int secondY2, int rad, int cx, int cy){
-    // https://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect
+bool verifyLineIntersect(int firstX1, int firstY1, int firstX2, int firstY2,
+                         int secondX1, int secondY1, int secondX2, int secondY2) {
     // check if lines are parallel
     if ((firstX1 - firstX2) * (secondY1 - secondY2) == (firstY1 - firstY2) * (secondX1 - secondX2)) {
         return false;
     }
-    // check if lines are colinear
+    // check if lines are collinear
     if ((firstX1 - firstX2) * (secondY1 - firstY1) == (firstY1 - firstY2) * (secondX1 - firstX1)) {
         return false;
     }
     // check if lines intersect
     if ((firstX1 - firstX2) * (secondY1 - firstY1) - (firstY1 - firstY2) * (secondX1 - firstX1) < 0) {
-        pair<int, int> intersect;
-        intersect.first = ((firstX1 * firstY2 - firstY1 * firstX2) * (secondX1 - secondX2) - (firstX1 - firstX2) * (secondX1 * secondY2 - secondY1 * secondX2)) / ((firstX1 - firstX2) * (secondY1 - secondY2) - (firstY1 - firstY2) * (secondX1 - secondX2));
-        intersect.second = ((firstX1 * firstY2 - firstY1 * firstX2) * (secondY1 - secondY2) - (firstY1 - firstY2) * (secondX1 * secondY2 - secondY1 * secondX2)) / ((firstX1 - firstX2) * (secondY1 - secondY2) - (firstY1 - firstY2) * (secondX1 - secondX2));
-        if (getDist(intersect.first, intersect.second, cx, cy) <= rad){
-            return true;
-        }
+        return true;
     }
     return false; // If none of the above conditions are met, the lines do not intersect
+}
+
+pair<double, double> calcIntersectionPoint(int firstX1, int firstY1, int firstX2, int firstY2,
+                                           int secondX1, int secondY1, int secondX2, int secondY2) {
+    // https://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect
+    pair<double, double> intersect;
+    intersect.first = (double)((firstX1 * firstY2 - firstY1 * firstX2) * (secondX1 - secondX2)
+                               - (firstX1 - firstX2) * (secondX1 * secondY2 - secondY1 * secondX2))
+                      / ((firstX1 - firstX2) * (secondY1 - secondY2) - (firstY1 - firstY2) * (secondX1 - secondX2));
+    intersect.second = (double)((firstX1 * firstY2 - firstY1 * firstX2) * (secondY1 - secondY2)
+                                - (firstY1 - firstY2) * (secondX1 * secondY2 - secondY1 * secondX2))
+                       / ((firstX1 - firstX2) * (secondY1 - secondY2) - (firstY1 - firstY2) * (secondX1 - secondX2));
+    return intersect;
 }
 
 // Read a line of variable length/inputs and return a vector of ints
@@ -116,9 +133,13 @@ vector<int> lineReaderInt(int numInputs, ifstream& in){
     string line;
     getline(in, line);
     for (int i = 0; i < numInputs; i++) {
-        if (line.size() == 1) inputs.push_back(stoi(line.substr(0, line.size())));
-        inputs.push_back(stoi(line.substr(0, line.find(' '))));
-        line.erase(0, line.find(' ') + 1);
+        if (line.size() == 1) {
+            inputs.push_back(stoi(line.substr(0, line.size())));
+        }
+        else {
+            inputs.push_back(stoi(line.substr(0, line.find(' '))));
+            line.erase(0, line.find(' ') + 1);
+        }
     }
     return inputs;
 }
